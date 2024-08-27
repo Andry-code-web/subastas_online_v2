1e442bac519c008c154a269cafabd761bad9841f
Andry-code-web <73274663@certus.edu.pe>
2024-08-27 11:32:45 -0500

    responcive_home_V1

---
+++ public/js/partials/script_header.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const activarMenu = document.getElementById('menu_activar');
+    const cerrarMenu = document.getElementById('menu_cerrar');
+    const conterMenu = document.getElementById('menu_navegador');
+
+    activarMenu.addEventListener('click', () => {
+        conterMenu.classList.add('active');
+    });
+
+    cerrarMenu.addEventListener('click', () => {
+        conterMenu.classList.remove('active');
+    });
+})
